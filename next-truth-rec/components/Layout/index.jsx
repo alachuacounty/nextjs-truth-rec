0974ca44da3ba3d238c3379c1f374d09cf6ab213
ashvini-patel <patel.mashvini@gmail.com>
2020-05-21 14:02:21 -0400

    layout styles global

--- next-truth-rec/components/Layout/index.jsx
+++ next-truth-rec/components/Layout/index.jsx
@@ -17,7 +17,7 @@ class Layout extends React.Component {
             <Modal/>
             {this.props.children}
             <Footer/>
-            <style jsx>
+            <style jsx global>
                 {layoutStyles}
             </style>
             <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
