6f60b5f199af1d10ad133b5cbc6170f1c37787f9
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2020-08-05 16:30:13 -0400

    Updating next.js project

--- next-truth-rec/pages/memberspage/members.jsx
+++ next-truth-rec/pages/memberspage/members.jsx
@@ -1,14 +1,14 @@
 import Layout from "../../components/Layout";
-import memberStyles from "./members-styles";
+import membersStyles from "./members-styles";
 class MembersPage extends React.Component{
     render(){
         return(
     <Layout>
         
-            <style jsx>{memberStyles}</style>
+           
      </Layout>
     )
         }
 }
 
-export default MediaPage;
+export default MembersPage;
