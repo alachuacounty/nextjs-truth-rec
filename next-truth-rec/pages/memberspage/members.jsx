4b9a5aa1d4aeb433ab0b960eeee94db252516588
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2020-08-05 15:59:38 -0400

    creating members page in next.js

---
+++ next-truth-rec/pages/memberspage/members.jsx
@@ -0,0 +1,14 @@
+import Layout from "../../components/Layout";
+import memberStyles from "./members-styles";
+class MembersPage extends React.Component{
+    render(){
+        return(
+    <Layout>
+        
+            <style jsx>{memberStyles}</style>
+     </Layout>
+    )
+        }
+}
+
+export default MediaPage;
