b68984b1bec58eac80937c0e4b18eefcad81090a
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2020-08-11 12:44:49 -0400

    finished updating content for the events page

--- next-truth-rec/pages/Events/index.jsx
+++ next-truth-rec/pages/Events/index.jsx
@@ -40,7 +40,7 @@ render(){
                 </div>
               <hr/>
              
-                <div>
+              <div>
                     <h3><a href="https://www.hailehomestead.org/"><strong>February 15, 2020 - Reclaiming Kin: Once Lost, Now Found</strong></a></h3>
                     <p>On February 15th at 10 am the Allen & Ethel Graham Visitors Center and Museum at the Historic Haile Homestead, the newest exhibit "Reclaiming Kin: Once Lost, Now Found," opened to the public for the first time! Tours of the Homestead were conducted at 10:15 am, 11:15 am, 12:15 pm and 1:15 pm.</p>
                     <h4>Related Media &amp; News link </h4>
@@ -48,54 +48,84 @@ render(){
                 </div>
                     <hr/>
                     <div>
-                        <h3><a href="https://rosewoodflorida.com/"><strong>2/13/2020 - Reclaiming Kin:  The Legacy of Hampton and Grace Hathcock</strong></a></h3>
-                        <p>Join us for a talk on Thursday, February 13th at 7 pm for "Reclaiming Kin: The Legacy of Hampton &amp; Grace Hathcock," by Tatanya Peterson, Hathcock descendant and genealogist, and Karen Kirkman, Historian at Kanapaha Presbyterian Church, 6221 SW 75th Terrace, Gainesville.</p>
-                        <p>Peterson will share how she traced her family line back to Hampton and Grace Hathcock, her 4X great grandparents, who were enslaved on the plantation of James Chesnut, near Alachua, Florida. Learn about the discoveries she and Kirkman made, and the resources they both used in their research. FREE and open to the public; refreshments following talk.</p>
-                        <br/><a href="https://rosewoodflorida.com/">Learn More &gt;</a>
-                    </div>
+                    <h3><a href="https://rosewoodflorida.com/"><strong>Febuary 13, 2020 - Reclaiming Kin:  The Legacy of Hampton and Grace Hathcock</strong></a></h3>
+                    <p> Thursday, February 13th at 7 pm for "Reclaiming Kin: The Legacy of Hampton & Grace Hathcock," by Tatanya Peterson, Hathcock descendant and genealogist, and Karen Kirkman, Historian at Kanapaha Presbyterian Church, 6221 SW 75th Terrace, Gainesville.</p>
+                    <p>Peterson shared how she traced her family line back to Hampton and Grace Hathcock, her 4X great grandparents, who were enslaved on the plantation of James Chesnut, near Alachua, Florida. Learn about the discoveries she and Kirkman made, and the resources they both used in their research.</p>
+                    <br />
+                    <h4>Related Media &amp; News link </h4>
+                    <a href="https://rosewoodflorida.com/">About Rosewood </a>
+                    
+                </div>
                     <hr/>
                     
                     <div>
-                        <h3><a href="https://www.alachuacounty.us/news/Article/Pages/Memorial-Service-for-the-Victims-of-Lynching-in-Alachua-County.aspx"><strong>2/7/2020 - Memorial Service for the Victims of Lynching in Alachua County</strong></a></h3>
-                        <p>Alachua County is hosting a Memorial Service for the Victims of Lynching in Alachua County on
-                            Friday, February 7, 2020, beginning at 6 p.m. at Mount Pleasant United Methodist Church (630
-                            N.W. 2nd Street, Gainesville). The event is free and open to the public.</p><br/>
-                            <a href="https://www.alachuacounty.us/news/Article/Pages/Memorial-Service-for-the-Victims-of-Lynching-in-Alachua-County.aspx">Learn
-                                More &gt;</a>
-                    </div>
+                    <h3><a href="https://www.wcjb.com/content/news/Alachua-County-holds-a-567693351.html"><strong>February 7, 2020 - Memorial Service for the Victims of Lynching in Alachua County</strong></a></h3>
+                    <p>Alachua County is hosted a Memorial Service for the Victims who were Lynched in Alachua County on
+                        Friday, February 7, 2020,at Mount Pleasant United Methodist Church (630
+                        N.W. 2nd Street, Gainesville). For more infromation follow the link and to watch the memorial click on the video below </p><br />
+                        <h4>Related Media &amp; News link </h4>
+                        <a href="https://www.wuft.org/news/2020/02/08/alachua-county-begins-truth-and-reconciliation-process-with-memorial-for-lynching-victims/">Wuft-Alachua County Advances Truth And Reconciliation Process With Memorial For Lynching Victims </a>
+                        <br/>
+                        <a href="https://soundcloud.com/wuft/alachua-countys-lynching-memorial-service">WUFT Sound Cloud- Alachua County Lynching Memorial Service</a>
+                        <br/>
+                            
+                            <a href="https://youtu.be/h7ue8it-R0s">Youtube Video- Victims of Lynching Memorial Service</a>
+                </div>
                     <hr/>
                     <div>
-                        <h3><strong>2/3/2020 - Truth &amp; Reconciliation Update and Presentation to joint meeting of City of Gainesville/County Commissions</strong></h3>
-                    </div>
+                    <h3><a href="https://www.wuft.org/news/2020/02/06/alachua-county-to-host-truth-and-reconciliation-events-remembering-lynching-victims/"><strong>February 2, 2020 - Truth &amp; Reconciliation Update and Presentation to joint meeting of City of Gainesville/County Commissions</strong></a></h3>
+                    <p>Alachua County and the City of Gainesville met together on Febuary 2, 2020 to discuss the proper actions to recognize those who were lynched in our area.</p>
+                    <h4>Related Media &amp; News link </h4>
+                    <a href="https://www.wuft.org/news/2020/02/06/alachua-county-to-host-truth-and-reconciliation-events-remembering-lynching-victims/">WUFT-Alachua County To Host Truth And Reconciliation Events Remembering Lynching Victims </a>
+                </div>
                     <hr/>
                     <div>
-                        <h3><strong>1/28/2020 - Truth &amp; Reconciliation Update and Presentation to County Commission</strong></h3>
-                    </div>
+                    <h3><a href="https://www.gainesville.com/news/20180627/county-effort-aims-to-make-peace-with-past"><strong>January 28, 2020- Truth &amp; Reconciliation Update and Presentation to County Commission</strong></a></h3>
+                    <p>On January 28th 2020, alachua county presented Truth and Reconciliation.</p>
+                    <h4>Related Media &amp; News link </h4>
+                    <a href="https://www.gainesville.com/news/20180627/county-effort-aims-to-make-peace-with-past">Gainesville Sun-County effort aims to make peace with past </a>
+
+                </div>
                     <hr/>
                     <div>
-                        <h3><strong>1/1/2020 -  “Rosewood Day” Proclamation &amp; Candle Light Ceremony</strong></h3>
-                        <p>At Rosewood Historic Marker in adjoining Levy County; the history impacted Alachua County with survivors fleeing to Archer</p><br/><a href="https://rosewoodflorida.com/">Learn more &gt;</a>
-                    </div>
+                    <h3><a href="https://rosewoodflorida.com/history/"><strong>January 1, 2020 -  “Rosewood Day” Proclamation & Candle Light Ceremony</strong></a></h3>
+                    <p>At Rosewood Historic Marker in adjoining Levy County; the history impacted Alachua County with survivors fleeing to Archer</p><br />
+                    <h4>Related Media &amp; News linke </h4>
+                    <a href="https://youtu.be/O-GL1pMMhUg">Youtube Video- 'Rosewood Day' candlelight ceremony</a>
+                </div>
                     <hr/>
                     <div>
-                        <h3><strong>12/10/2019 - Presentation on the history of the Newberry Lynchings of 1916, by UF historian Dr. Tricia Hilliard Nunn.&nbsp; Concerned Citizens of Newberry</strong></h3>
-                    </div>
+                    <h3><a href="https://www.wuft.org/news/2018/12/28/2018-was-the-year-alachua-county-began-to-reckon-with-its-history-of-lynching/"><strong>December 10, 2019 - Presentation on the history of the Newberry Lynchings of 1916, by UF historian Dr. Patricia Hilliard Nunn.  Concerned Citizens of Newberry</strong></a></h3>
+                    <h4>Related Media &amp; News link </h4>
+                    <a href="https://youtu.be/O-GL1pMMhUg">Youtube Video- WUFT-Panel discussion: Alachua County reflects on its lynching history</a>
+
+                </div>
                     <hr/>
                     <div>
-                        <h3><strong>9/18/2019 - State Historic Marker Dedication, Newberry Lynchings of 1916 (text by Dr. Tricia Hilliard Nunn).&nbsp; At Pleasant Plain UMC, Newberry</strong></h3>
-                    </div>
+                    <h3><a href="http://gm5-lkweb.newscyclecloud.com/news/20190427/historical-marker-denoting-newberry-lynching-of-1916-unveiled-saturday"><strong>September 18, 2019 - State Historic Marker Dedication, Newberry Lynchings of 1916 (text by Dr. Tricia Hilliard Nunn).  At Pleasant Plain UMC, Newberry</strong></a></h3>
+                    <p>Descendants of the Newberry Six gathered for the unveiling of an educational marker on church grounds.</p>
+                    <h4>Related Media &amp; News link </h4>
+                    <a href="http://gm5-lkweb.newscyclecloud.com/news/20190427/historical-marker-denoting-newberry-lynching-of-1916-unveiled-saturday">Gainesville Sun-Historical marker denoting the Newberry Lynching of 1916 unveiled Saturday </a>
+                </div>
                     <hr/>
                     <div>
-                        <h3><strong>3/21/2019 - The African American Oral History Symposium, 'From Segregation to Black Lives Matter.' UF Samuel Proctor Oral History Program</strong></h3>
-                    </div>
+                    <h3><a href="https://www.alligator.org/news/uf-african-american-oral-history-symposium-uses-sound-to-connect-students-with-history/article_63d52eb8-4ec4-11e9-bced-4f4b699fc361.html"><strong>March 21, 2019 - The African American Oral History Symposium, 'From Segregation to Black Lives Matter.' UF Samuel Proctor Oral History Program</strong></a></h3>
+                    <h4>Related Media &amp; News link </h4>
+                    <a href="https://www.alligator.org/news/uf-african-american-oral-history-symposium-uses-sound-to-connect-students-with-history/article_63d52eb8-4ec4-11e9-bced-4f4b699fc361.html">Alligator-UF African American Oral History Symposium uses sound to connect students with history </a>
+                </div>
                     <hr/>
                     <div>
-                        <h3><strong>9/20/2018 - Reconciliation meeting of Newberry Lynching history. Concerned Citizens of Newberry</strong></h3>
-                    </div>
+                    <h3> <a href="https://www.wuft.org/news/2018/10/16/newberrys-north-and-south-two-sides-one-history-and-a-path-for-healing/"><strong>September 20, 2018 - Reconciliation meeting of Newberry Lynching history. Concerned Citizens of Newberry</strong></a></h3>
+                    <h4>Related Media &amp; News link </h4>
+                    <a href="https://www.wuft.org/news/2018/10/16/newberrys-north-and-south-two-sides-one-history-and-a-path-for-healing/">
+                        WUFT-Newberry’s North and South: Two Sides, One History And A Path For Healing </a>
+                </div>
                     <hr/>
                     <div>
-                        <h3><strong>7/17/2018 - Discussion of Newberry Lynching history. Concerned Citizens of Newberry</strong></h3>
-                    </div>
+                    <h3><a href="https://www.gainesville.com/news/20180718/newberry-first-in-lynching-truth-process"><strong>July 17, 2018 - Discussion of Newberry Lynching history. Concerned Citizens of Newberry</strong></a></h3>
+                    <h4>Related Media &amp; News link </h4>
+                    <a href="https://www.gainesville.com/news/20180718/newberry-first-in-lynching-truth-process">Gainesville Sun- Newberry's First in Lynching Truth Process </a>
+                </div>
                     <hr/>
                     <div>
                         <h3><strong>10/24/2017 - 'An Outrage, Film viewing and Panel Discussion. Matheson History Museum</strong></h3>
